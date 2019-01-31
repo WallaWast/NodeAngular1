6965736d86132c7b0c3b1be1e5d345e8590388a5
Wast <wallacewst@gmail.com>
2019-01-31 10:42:32 -0200

    Melhorias navegação

--- angular1/app/billingCycle/billingCycleControler.js
+++ angular1/app/billingCycle/billingCycleControler.js
@@ -2,20 +2,43 @@
     angular.module('primeiraApp').controller('BillingCycleCtrl', [
         '$http',
         'msgs',
+        'tabs',
         BillingCycleController
     ])
 
-    function BillingCycleController($http, msgs) {
+    function BillingCycleController($http, msgs, tabs) {
         const vm = this
+        const url = 'http://localhost:3003/api/billingCycles'
+
+        vm.refresh = function(){
+            $http.get(url).then(function(response){
+                vm.billingCycle = {}
+                vm.billingCycles = response.data
+                tabs.show(vm, {tabList: true, tabCreate: true})
+            }).catch(function(resp){
+                msgs.addError(resp.data.errors)
+            })
+        }
 
         vm.create = function () {
-            const url = 'http://localhost:3003/api/billingCycles'
             $http.post(url, vm.billingCycle).then(function (response) {
-                vm.billingCycle = {}
+                vm.refresh()
                 msgs.addSuccess('Operação realizada com sucesso!')
             }).catch(function(resp){
                 msgs.addError(resp.data.errors)
             })
         }
+
+        vm.showTabUpdate = function(billingCycle){
+            vm.billingCycle = billingCycle
+            tabs.show(vm, {tabUpdate: true})
+        }
+
+        vm.showTabDelete = function(billingCycle){
+            vm.billingCycle = billingCycle
+            tabs.show(vm, {tabDelete: true})
+        }
+
+        vm.refresh()
     }
 })()
